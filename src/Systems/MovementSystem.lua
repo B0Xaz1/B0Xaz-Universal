f8d7891baafa6c33aa4a36d52e1b1c250f6a5e3d
B0Xaz <bradleycessna71@gmail.com>
2026-08-21 19:16:45 -0400

    Update MovementSystem.lua

--- src/Systems/MovementSystem.lua
+++ src/Systems/MovementSystem.lua
@@ -1,5 +1,6 @@
 -- src/Systems/MovementSystem.lua
 return function(Context)
+    local UIS = game:GetService("UserInputService")
     local FeatureConfig = Context.FeatureConfig
     local Utils = Context.Utils
 
@@ -8,11 +9,14 @@ return function(Context)
 
     function MovementSystem.UpdateBhop()
         if not FeatureConfig.Movement or not FeatureConfig.Movement.Bhop then return end
+        
+        -- Require spacebar hold on desktop or active jump trigger
+        if not UIS:IsKeyDown(Enum.KeyCode.Space) and not UIS.TouchEnabled then return end
 
         local hum = Utils.GetHumanoid()
         if not hum or hum.Health <= 0 then return end
 
-        if tick() - lastJumpTime < 0.25 then return end
+        if tick() - lastJumpTime < 0.2 then return end
 
         local state = hum:GetState()
         local isGrounded = (hum.FloorMaterial ~= Enum.Material.Air)
@@ -28,20 +32,17 @@ return function(Context)
 
     function MovementSystem.UpdateCFrameSpeed(dt)
         if not FeatureConfig.Movement or not FeatureConfig.Movement.CFrameSpeed then return end
+        if FeatureConfig.Movement.FlyEnabled then return end
 
         local hum = Utils.GetHumanoid()
         local root = Utils.GetRootPart()
         if not hum or not root or hum.Health <= 0 then return end
-        if FeatureConfig.Movement.FlyEnabled then return end
 
         local moveDir = hum.MoveDirection
         if moveDir.Magnitude < 0.05 then return end
 
-        local targetSpeed = FeatureConfig.Movement.CFrameSpeedValue or 50
-        local currentWalkSpeed = hum.WalkSpeed
-        local deltaSpeed = targetSpeed - currentWalkSpeed
-
-        root.CFrame = root.CFrame + (moveDir * deltaSpeed * (dt or 0.016))
+        local speed = FeatureConfig.Movement.CFrameSpeedValue or 50
+        root.CFrame = root.CFrame + (moveDir * speed * (dt or 0.016))
     end
 
     function MovementSystem.Update(dt)
