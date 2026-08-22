e5ad5d3f08829b28af98abbc44d3a13ac00bf91b
B0Xaz <bradleycessna71@gmail.com>
2026-08-22 17:30:34 -0400

    Simplify key list return in Entry.lua

--- src/Keys/Entry.lua
+++ src/Keys/Entry.lua
@@ -1,14 +1,1 @@
-local SETTINGS = {
-	KEYS = {
-		"ENTRY-ALPHA-01",
-		"ENTRY-BETA-02",
-		"ENTRY-TEST-KEY",
-	},
-}
-
-local keyList = table.create(#SETTINGS.KEYS)
-for index, key in ipairs(SETTINGS.KEYS) do
-	keyList[index] = key
-end
-
-return keyList
+return { "ENTRY-ALPHA-01", "ENTRY-BETA-02", "ENTRY-TEST-KEY", "ENTRY-ACCESS" }
