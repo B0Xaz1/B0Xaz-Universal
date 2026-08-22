6b71ea491d952c6904bfe85f8b4585d4079199e8
B0Xaz <bradleycessna71@gmail.com>
2026-08-22 17:30:42 -0400

    Update Normal.lua

--- src/Keys/Normal.lua
+++ src/Keys/Normal.lua
@@ -1,14 +1,1 @@
-local SETTINGS = {
-	KEYS = {
-		"NORMAL-ALPHA-01",
-		"NORMAL-BETA-02",
-		"NORMAL-TEST-KEY",
-	},
-}
-
-local keyList = table.create(#SETTINGS.KEYS)
-for index, key in ipairs(SETTINGS.KEYS) do
-	keyList[index] = key
-end
-
-return keyList
+return { "NORMAL-ALPHA-01", "NORMAL-BETA-02", "NORMAL-TEST-KEY", "NORMAL-ACCESS" }
