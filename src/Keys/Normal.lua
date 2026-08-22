558c7afa028c75419a5a6e438c382d6354aae645
B0Xaz <bradleycessna71@gmail.com>
2026-08-22 14:45:26 -0400

    Refactor key storage to use SETTINGS table

--- src/Keys/Normal.lua
+++ src/Keys/Normal.lua
@@ -1,6 +1,14 @@
--- src/Keys/Normal.lua
-return {
-	"NORMAL-ALPHA-01",
-	"NORMAL-BETA-02",
-	"NORMAL-TEST-KEY",
+local SETTINGS = {
+	KEYS = {
+		"NORMAL-ALPHA-01",
+		"NORMAL-BETA-02",
+		"NORMAL-TEST-KEY",
+	},
 }
+
+local keyList = table.create(#SETTINGS.KEYS)
+for index, key in ipairs(SETTINGS.KEYS) do
+	keyList[index] = key
+end
+
+return keyList
