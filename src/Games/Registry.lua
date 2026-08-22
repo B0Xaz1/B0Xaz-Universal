99767a2ee3834280898865bd07e6593eaf784a45
B0Xaz <bradleycessna71@gmail.com>
2026-08-22 17:31:08 -0400

    Update Registry.lua

--- src/Games/Registry.lua
+++ src/Games/Registry.lua
@@ -1,24 +1,10 @@
 -- // src/Games/Registry.lua
-local SETTINGS = {
-	REGISTRY = {
+return function()
+	return {
 		["155615604"] = {
 			Name = "Prison Life",
 			Folder = "155615604",
-			Description = "Prison Life automated door phasing, warp gun grabber, and weapon mods",
+			Description = "Door phasing, gun grabbers, weapon mods",
 		},
-	},
-}
-
-return function()
-	local registryCopy = {}
-	for placeId, config in pairs(SETTINGS.REGISTRY) do
-		local idStr = tostring(placeId)
-		local data = {
-			Name = config.Name,
-			Folder = config.Folder or idStr,
-			Description = config.Description or "",
-		}
-		registryCopy[idStr] = data
-	end
-	return registryCopy
+	}
 end
