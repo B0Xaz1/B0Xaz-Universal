f8db741ffdbcf91fb73ad73df1b42e7d1346182b
B0Xaz <bradleycessna71@gmail.com>
2026-08-25 23:29:19 -0400

    Update Section.lua

--- Root/UI/Components/Section.lua
+++ Root/UI/Components/Section.lua
@@ -13,7 +13,7 @@ function Section.new(parent, title, themeEngine)
 	local theme = themeEngine.Current
 
 	self.Frame = DOM.Create("Frame", {
-		Size = UDim2.new(1, 0, 0, 30),
+		Size = UDim2.new(1, 0, 0, 34),
 		BackgroundTransparency = 1,
 		Parent = parent,
 	})
@@ -40,7 +40,7 @@ function Section.new(parent, title, themeEngine)
 
 	self.Line = DOM.Create("Frame", {
 		Size = UDim2.new(1, -20, 0, 1),
-		Position = UDim2.new(0, 10, 0, 24),
+		Position = UDim2.new(0, 10, 0, 28),
 		BackgroundColor3 = theme.BorderDim,
 		BorderSizePixel = 0,
 		Parent = self.Frame,
